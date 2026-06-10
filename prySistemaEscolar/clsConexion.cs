6fb40db533aa84575f412c8cd51926133d480edb
ligoh <ligoh@DELL>
2026-06-10 17:41:02 -0600

    clase conexion

--- prySistemaEscolar/clsConexion.cs
+++ prySistemaEscolar/clsConexion.cs
@@ -8,6 +8,7 @@ namespace prySistemaEscolar
 {
     internal class clsConexion
     {
+        //Conexion con la BD
         private string host = "189.240.192.140";
         private string bd = "bdescuela3b";
         private string usuario = "terceroB";
@@ -16,6 +17,7 @@ namespace prySistemaEscolar
 
         private string cadenaConexion => $"server = {host};database ={bd};user = {usuario};password = {password};port = {puerto};";
 
+        //Abre la conexion con la BD
         public MySqlConnection AbrirConexion()
         {
             var conexion = new MySqlConnection(cadenaConexion);
@@ -25,11 +27,12 @@ namespace prySistemaEscolar
                 conexion.Open();
                 return conexion;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception("Error al intentar conectarse a la base de datos" + ex.Message, ex);
             }
         }
+        //Cierra la conexion con la BD
         public void CerrarConexion(MySqlConnection conexion)
         {
             try
@@ -38,13 +41,13 @@ namespace prySistemaEscolar
                 {
                     conexion.Close();
                     conexion.Dispose();
-                } 
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception("Error al cerrar la conexion con la bse de datos: " + ex.Message, ex);
             }
-            
+
         }
  
     }
