01439e9238acebb830350132c839efeaf920f8dc
ligoh <ligoh@DELL>
2026-06-15 08:26:50 -0600

    act

--- prySistemaEscolar/frmPrincipal.cs
+++ prySistemaEscolar/frmPrincipal.cs
@@ -40,12 +40,7 @@ namespace prySistemaEscolar
             pcbCarreras.BackColor = Color.Transparent;
             pcbDocentes.BackColor = Color.Transparent;
             pcbUsuarios.BackColor = Color.Transparent;
-            pcbSalir.BackColor = Color.Transparent;
-
-            //verificando permisos
-            pcbCarreras.Enabled = clsLogin.EsAdministrador;
-            pcbDocentes.Enabled = clsLogin.EsAdministrador;
-            
+            pcbSalir.BackColor = Color.Transparent; 
 
         }
     }
