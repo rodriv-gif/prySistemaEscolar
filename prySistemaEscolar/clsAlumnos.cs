7f04a5908d5acbcb560cb01cd2da692b00eb9b89
BRYAN BENITEZ <20251077@uthh.edu.mx>
2026-06-29 18:48:51 -0600

    bryan se elavoro la segunda parte del codigo

---
+++ prySistemaEscolar/clsAlumnos.cs
@@ -0,0 +1,40 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace prySistemaEscolar
+{
+    internal class clsAlumnos
+    {
+        private int matricula;
+        private string nombreAlumno;
+        private string apellidoP;
+        private string apellidoM;
+        private string direccion;
+        private string telefono;
+        private string correo;
+        private string promedioBachillerato;
+        private int idTutor;//foraneo
+        private int idCarrera;//foraneo
+        private int idUsuario;
+
+
+        //Adaptador y tabla virtuales de la clase
+        private MySqlDataAdapter consultar;
+        private DataTable tabla;
+        //propiedades
+        public int Matricula { get => matricula; set => matricula = value; }
+        public string NombreAlumno { get => nombreAlumno; set => nombreAlumno = value; }
+        public string ApellidoP { get => apellidoP; set => apellidoP = value; }
+        public string ApellidoM { get => apellidoM; set => apellidoM = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string PromedioBachillerato { get => promedioBachillerato; set => promedioBachillerato = value; }
+        public int IdTutor { get => idTutor; set => idTutor = value; }
+        public int IdCarrera { get => idCarrera; set => idCarrera = value; }
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+    }
+}
