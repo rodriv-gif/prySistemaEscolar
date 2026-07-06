1c904ab5a15fb4064a9ef349ea914f399e2f306d
Jesus Alexis <20251037@uthh.edu.mx.>
2026-07-06 16:00:38 -0600

    Se creo el formulario frmDocente, primeras bases para la programacion

--- prySistemaEscolar/Properties/Resources.Designer.cs
+++ prySistemaEscolar/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace prySistemaEscolar.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Docente {
+            get {
+                object obj = ResourceManager.GetObject("Docente", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Eliminar {
             get {
                 object obj = ResourceManager.GetObject("Eliminar", resourceCulture);
