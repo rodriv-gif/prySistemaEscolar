f25eb2f346009135627f9cbcfff9f7d1a377c75d
uriele-boop <20250997@uthh.edu.mx>
2026-07-07 22:45:41 -0600

    Hice el codigo para los botones de nuevo, guardar y eliminar, en el formulario de docentes, usando eventos para que funcionen los botones

--- prySistemaEscolar/frmDocente.Designer.cs
+++ prySistemaEscolar/frmDocente.Designer.cs
@@ -65,9 +65,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.Docente;
-            pictureBox1.Location = new Point(635, 3);
+            pictureBox1.Location = new Point(556, 2);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(143, 116);
+            pictureBox1.Size = new Size(125, 87);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -76,9 +77,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(361, 18);
+            label1.Location = new Point(316, 14);
             label1.Name = "label1";
-            label1.Size = new Size(268, 81);
+            label1.Size = new Size(215, 65);
             label1.TabIndex = 1;
             label1.Text = "Docente";
             // 
@@ -89,75 +90,87 @@
             panel1.Controls.Add(textBox3);
             panel1.Controls.Add(textBox2);
             panel1.Controls.Add(textBox1);
-            panel1.Location = new Point(124, 134);
+            panel1.Location = new Point(108, 100);
+            panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
             panel1.Size = new Size(0, 0);
             panel1.TabIndex = 2;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(220, 227);
+            textBox5.Location = new Point(192, 170);
+            textBox5.Margin = new Padding(3, 2, 3, 2);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(500, 27);
+            textBox5.Size = new Size(438, 23);
             textBox5.TabIndex = 4;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(220, 170);
+            textBox4.Location = new Point(192, 128);
+            textBox4.Margin = new Padding(3, 2, 3, 2);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(500, 27);
+            textBox4.Size = new Size(438, 23);
             textBox4.TabIndex = 3;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(220, 116);
+            textBox3.Location = new Point(192, 87);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(500, 27);
+            textBox3.Size = new Size(438, 23);
             textBox3.TabIndex = 2;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(220, 69);
+            textBox2.Location = new Point(192, 52);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(500, 27);
+            textBox2.Size = new Size(438, 23);
             textBox2.TabIndex = 1;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(220, 19);
+            textBox1.Location = new Point(192, 14);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(500, 27);
+            textBox1.Size = new Size(438, 23);
             textBox1.TabIndex = 0;
             // 
             // btnNuevo
             // 
             btnNuevo.BackgroundImage = Properties.Resources.Nuevo;
             btnNuevo.BackgroundImageLayout = ImageLayout.Stretch;
-            btnNuevo.Location = new Point(361, 462);
+            btnNuevo.Location = new Point(316, 346);
+            btnNuevo.Margin = new Padding(3, 2, 3, 2);
             btnNuevo.Name = "btnNuevo";
-            btnNuevo.Size = new Size(135, 73);
+            btnNuevo.Size = new Size(118, 55);
             btnNuevo.TabIndex = 3;
             btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click;
             // 
             // btnGuardar
             // 
             btnGuardar.BackgroundImage = Properties.Resources.Guardar;
             btnGuardar.BackgroundImageLayout = ImageLayout.Stretch;
-            btnGuardar.Location = new Point(591, 460);
+            btnGuardar.Location = new Point(517, 345);
+            btnGuardar.Margin = new Padding(3, 2, 3, 2);
             btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(135, 73);
+            btnGuardar.Size = new Size(118, 55);
             btnGuardar.TabIndex = 4;
             btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
             // 
             // btnEliminar
             // 
             btnEliminar.BackgroundImage = Properties.Resources.Eliminar;
             btnEliminar.BackgroundImageLayout = ImageLayout.Stretch;
-            btnEliminar.Location = new Point(814, 460);
+            btnEliminar.Location = new Point(712, 345);
+            btnEliminar.Margin = new Padding(3, 2, 3, 2);
             btnEliminar.Name = "btnEliminar";
-            btnEliminar.Size = new Size(135, 73);
+            btnEliminar.Size = new Size(118, 55);
             btnEliminar.TabIndex = 5;
             btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
             // 
             // dgvDocentes
             // 
@@ -180,11 +193,12 @@
             dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
             dgvDocentes.DefaultCellStyle = dataGridViewCellStyle2;
             dgvDocentes.EnableHeadersVisualStyles = false;
-            dgvDocentes.Location = new Point(176, 575);
+            dgvDocentes.Location = new Point(154, 431);
+            dgvDocentes.Margin = new Padding(3, 2, 3, 2);
             dgvDocentes.Name = "dgvDocentes";
             dgvDocentes.RowHeadersWidth = 60;
             dgvDocentes.RowTemplate.Height = 40;
-            dgvDocentes.Size = new Size(963, 162);
+            dgvDocentes.Size = new Size(843, 122);
             dgvDocentes.TabIndex = 6;
             dgvDocentes.SelectionChanged += dgvDocentes_SelectionChanged;
             // 
@@ -192,9 +206,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(176, 121);
+            label2.Location = new Point(154, 91);
             label2.Name = "label2";
-            label2.Size = new Size(91, 28);
+            label2.Size = new Size(74, 21);
             label2.TabIndex = 7;
             label2.Text = "Docente";
             // 
@@ -202,9 +216,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(681, 121);
+            label3.Location = new Point(596, 91);
             label3.Name = "label3";
-            label3.Size = new Size(97, 31);
+            label3.Size = new Size(81, 25);
             label3.TabIndex = 8;
             label3.Text = "Usuario";
             // 
@@ -216,54 +230,60 @@
             pnlDocente.Controls.Add(txtPuesto);
             pnlDocente.Controls.Add(txtNombreDocente);
             pnlDocente.Controls.Add(txtClave);
-            pnlDocente.Location = new Point(176, 155);
+            pnlDocente.Location = new Point(154, 116);
+            pnlDocente.Margin = new Padding(3, 2, 3, 2);
             pnlDocente.Name = "pnlDocente";
-            pnlDocente.Size = new Size(428, 301);
+            pnlDocente.Size = new Size(374, 226);
             pnlDocente.TabIndex = 9;
             // 
             // txtCorreo
             // 
             txtCorreo.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtCorreo.Location = new Point(72, 239);
+            txtCorreo.Location = new Point(63, 179);
+            txtCorreo.Margin = new Padding(3, 2, 3, 2);
             txtCorreo.Name = "txtCorreo";
             txtCorreo.PlaceholderText = "Correo";
-            txtCorreo.Size = new Size(277, 38);
+            txtCorreo.Size = new Size(243, 32);
             txtCorreo.TabIndex = 4;
             // 
             // txtTelefono
             // 
             txtTelefono.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtTelefono.Location = new Point(72, 182);
+            txtTelefono.Location = new Point(63, 136);
+            txtTelefono.Margin = new Padding(3, 2, 3, 2);
             txtTelefono.Name = "txtTelefono";
             txtTelefono.PlaceholderText = "Telefono";
-            txtTelefono.Size = new Size(277, 38);
+            txtTelefono.Size = new Size(243, 32);
             txtTelefono.TabIndex = 3;
             // 
             // txtPuesto
             // 
             txtPuesto.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtPuesto.Location = new Point(72, 130);
+            txtPuesto.Location = new Point(63, 98);
+            txtPuesto.Margin = new Padding(3, 2, 3, 2);
             txtPuesto.Name = "txtPuesto";
             txtPuesto.PlaceholderText = "Puesto";
-            txtPuesto.Size = new Size(277, 38);
+            txtPuesto.Size = new Size(243, 32);
             txtPuesto.TabIndex = 2;
             // 
             // txtNombreDocente
             // 
             txtNombreDocente.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtNombreDocente.Location = new Point(72, 81);
+            txtNombreDocente.Location = new Point(63, 61);
+            txtNombreDocente.Margin = new Padding(3, 2, 3, 2);
             txtNombreDocente.Name = "txtNombreDocente";
             txtNombreDocente.PlaceholderText = "Nombre del docente";
-            txtNombreDocente.Size = new Size(277, 38);
+            txtNombreDocente.Size = new Size(243, 32);
             txtNombreDocente.TabIndex = 1;
             // 
             // txtClave
             // 
             txtClave.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtClave.Location = new Point(72, 29);
+            txtClave.Location = new Point(63, 22);
+            txtClave.Margin = new Padding(3, 2, 3, 2);
             txtClave.Name = "txtClave";
             txtClave.PlaceholderText = "Clave";
-            txtClave.Size = new Size(277, 38);
+            txtClave.Size = new Size(243, 32);
             txtClave.TabIndex = 0;
             // 
             // pnlUsuario
@@ -272,52 +292,57 @@
             pnlUsuario.Controls.Add(cmbPerfil);
             pnlUsuario.Controls.Add(txtPassword);
             pnlUsuario.Controls.Add(txtUsuario);
-            pnlUsuario.Location = new Point(682, 155);
+            pnlUsuario.Location = new Point(597, 116);
+            pnlUsuario.Margin = new Padding(3, 2, 3, 2);
             pnlUsuario.Name = "pnlUsuario";
-            pnlUsuario.Size = new Size(457, 301);
+            pnlUsuario.Size = new Size(400, 226);
             pnlUsuario.TabIndex = 10;
             // 
             // cmbPerfil
             // 
             cmbPerfil.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             cmbPerfil.FormattingEnabled = true;
-            cmbPerfil.Location = new Point(34, 130);
+            cmbPerfil.Location = new Point(30, 98);
+            cmbPerfil.Margin = new Padding(3, 2, 3, 2);
             cmbPerfil.Name = "cmbPerfil";
-            cmbPerfil.Size = new Size(401, 39);
+            cmbPerfil.Size = new Size(351, 33);
             cmbPerfil.TabIndex = 2;
             // 
             // txtPassword
             // 
             txtPassword.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtPassword.Location = new Point(34, 79);
+            txtPassword.Location = new Point(30, 59);
+            txtPassword.Margin = new Padding(3, 2, 3, 2);
             txtPassword.Name = "txtPassword";
             txtPassword.PlaceholderText = "Password";
-            txtPassword.Size = new Size(401, 38);
+            txtPassword.Size = new Size(351, 32);
             txtPassword.TabIndex = 1;
             // 
             // txtUsuario
             // 
             txtUsuario.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtUsuario.Location = new Point(34, 29);
+            txtUsuario.Location = new Point(30, 22);
+            txtUsuario.Margin = new Padding(3, 2, 3, 2);
             txtUsuario.Name = "txtUsuario";
             txtUsuario.PlaceholderText = "Usuario";
-            txtUsuario.Size = new Size(401, 38);
+            txtUsuario.Size = new Size(351, 32);
             txtUsuario.TabIndex = 0;
             // 
             // txtClaveDocente
             // 
             txtClaveDocente.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtClaveDocente.Location = new Point(810, 535);
+            txtClaveDocente.Location = new Point(709, 401);
+            txtClaveDocente.Margin = new Padding(3, 2, 3, 2);
             txtClaveDocente.Name = "txtClaveDocente";
             txtClaveDocente.PlaceholderText = "Buscar por clave";
-            txtClaveDocente.Size = new Size(329, 34);
+            txtClaveDocente.Size = new Size(288, 29);
             txtClaveDocente.TabIndex = 11;
             // 
             // frmDocente
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1315, 749);
+            ClientSize = new Size(1151, 562);
             Controls.Add(txtClaveDocente);
             Controls.Add(pnlUsuario);
             Controls.Add(pnlDocente);
@@ -330,6 +355,7 @@
             Controls.Add(panel1);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "frmDocente";
             Text = "frmDocente";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
--- prySistemaEscolar/frmDocente.cs
+++ prySistemaEscolar/frmDocente.cs
@@ -63,8 +63,86 @@ namespace prySistemaEscolar
             }
         }
 
-        
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            idClave = 0;
+            idUsuario = 0;
+            docentes.LimpiarPanel(pnlDocente);
+            docentes.LimpiarPanel(pnlUsuario);
+            txtClave.Focus();
+        }
 
-       
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int tipoOperacion = idClave == 0 ? 0 : 1;
+
+                docentes = new clsDocente();
+
+                docentes.Clave = int.Parse(txtClave.Text);
+                docentes.NombreDocente = string.IsNullOrEmpty(txtNombreDocente.Text) ? null : txtNombreDocente.Text;
+                docentes.Puesto = string.IsNullOrEmpty(txtPuesto.Text) ? null : txtPuesto.Text;
+                docentes.Telefono = string.IsNullOrEmpty(txtPuesto.Text) ? null : txtTelefono.Text;
+                docentes.Correo = string.IsNullOrEmpty(txtCorreo.Text) ? null : txtCorreo.Text;
+
+                //Propiedades del bloque de usuario
+                docentes.IdUsuario = idUsuario;
+                docentes.NombreUsuario = string.IsNullOrEmpty(txtUsuario.Text) ? null : txtUsuario.Text;
+                docentes.Password = string.IsNullOrEmpty(txtPassword.Text) ? null : txtPassword.Text;
+                docentes.Perfil = cmbPerfil.Text;
+
+                string msg = "";
+
+                //confirmacion de carrera
+                if (tipoOperacion == 1)
+                {
+                    var resp = MessageBox.Show("¿Confirmar que desea actualizar los datos de este docente?", "ALERTA", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (resp == DialogResult.Yes)
+                    {
+                        msg = docentes.GuardarActualizar(tipoOperacion);
+                        MessageBox.Show(msg, "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    msg = docentes.GuardarActualizar(tipoOperacion);
+                    MessageBox.Show(msg, "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                cargarGrid();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudieron guardar los datos:" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            var respuesta = MessageBox.Show($"¿Estás seguro de que deseas eliminar este Docente?", "ADVERTENCIA!", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+            if (respuesta == DialogResult.Yes)
+            {
+                try
+                {
+
+                    docentes = new clsDocente();
+
+                    docentes.Clave = idClave;
+                    docentes.IdUsuario = idUsuario;
+
+                    string resultado = docentes.Eliminar();
+
+                    MessageBox.Show(resultado, "Registro eliminado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    cargarGrid();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al eliminar el registro: " + ex.Message, "Error operacional", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
     }
 }
